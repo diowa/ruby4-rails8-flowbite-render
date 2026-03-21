d9b6a9ece566202b2621d7940a892fbc699b9e56
Geremia Taglialatela <tagliala.dev@gmail.com>
2026-03-21 18:53:14 +0100

    Update iconmap

--- config/iconmap.rb
+++ config/iconmap.rb
@@ -1,3 +1,7 @@
-# Pin icons by running ./bin/iconmap
+# frozen_string_literal: true
 
-pin "@fortawesome/fontawesome-free/svgs/brands/github.svg", to: "@fortawesome--fontawesome-free--svgs--brands--github.svg" # @7.2.0
+# Pin icons by running ./bin/iconmap
+pin '@fortawesome/fontawesome-free/svgs/brands/instagram.svg' # @7.2.0
+pin '@fortawesome/fontawesome-free/svgs/brands/github.svg' # @7.2.0
+pin 'heroicons/16/solid/academic-cap.svg' # @2.1.0
+pin 'heroicons/16/solid/adjustments-vertical.svg' # @2.1.0
