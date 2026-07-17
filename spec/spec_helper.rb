b145548e1e740a7b6c74de3da0336db3c5596f14
Geremia Taglialatela <tagliala@users.noreply.github.com>
2026-07-17 16:27:39 +0200

    Update dependencies (#324)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,15 +3,10 @@
 require 'simplecov'
 
 SimpleCov.start 'rails' do
-  if ENV['CI']
-    require 'simplecov-lcov'
+  if ENV['GITHUB_ACTIONS']
+    source_in_json false
 
-    SimpleCov::Formatter::LcovFormatter.config do |c|
-      c.report_with_single_file = true
-      c.single_report_path = 'coverage/lcov.info'
-    end
-
-    formatter SimpleCov::Formatter::LcovFormatter
+    formatter SimpleCov::Formatter::JSONFormatter
   end
 end
 
